8cccf337ad45e5e5d99d159b5e9825bdd010ff17
Jay Barbhaiya <jaybarb1990@gmail.com>
2022-01-06 18:22:16 +0100

    A simple Javascript, HTML and CSS game.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const host = "localhost";
+const port = "3000";
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+	res.setHeader("Content-Type", "text/html");
+	res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(port, host, () => {
+	console.log(`Server running on http://${host}:${port}`);
+});
